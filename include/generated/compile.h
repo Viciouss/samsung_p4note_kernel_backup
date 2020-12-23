68a8afc044eaf0f0cd79991d820b0bb24675e321
Martin Jücker <martin.juecker@gmail.com>
2020-12-23 21:00:14 +0100

    Dump of SHW-M480S_KOR_KK_Opensource

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Tue Dec 27 14:24:09 KST 2016"
+#define LINUX_COMPILE_BY "jell.lee"
+#define LINUX_COMPILE_HOST "jell"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
